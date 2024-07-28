bdd0e381465a0b58426a4b588f119c25b9703380
ivybm <134868955+ivybm@users.noreply.github.com>
2024-07-27 22:10:06 -0400

    Remove db connection as input to methods. Add GET /orders/{id}

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import sqlite3
 MAXIMUM_NAME_LENGTH = 64
 REQUIRED_PHONE_LENGTH = 10
 
+connection = sqlite3.connect('db.sqlite', check_same_thread=False)
+cursor= connection.cursor()
+
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="127.0.0.1", port=8080)
@@ -67,52 +70,43 @@ class OrderCreate(BaseModel):
 class OrderCreated(BaseModel):
     id: int
     timestamp: datetime
+    items: list[ItemQuantityTotalPrice]
+    total: float
+
+class OrderReturned(BaseModel):
+    id: int
+    timestamp: datetime
     name: str
     phone: str
+    notes: str
     items: list[ItemQuantityTotalPrice]
     total: float
 
 app = FastAPI()
 
-def get_db():
-    conn = sqlite3.connect('db.sqlite', check_same_thread=False)
-    try:
-        yield conn
-    finally:
-        conn.close()
-
-def get_customer_service(id: str, db: sqlite3.Connection = Depends(get_db)):
+def get_customer_service(id: str):
     """Retrieves a JSON representation of a customer in the DB"""
-    cursor = db.cursor()
-
     # Retrieve the customer
     cursor.execute("SELECT name, phone FROM customers where id = ?", (id,))
     data = cursor.fetchone()
-    cursor.close()
     if data is None:
-        return data
+        raise HTTPException(status_code=404, detail=f"Customer {id} not found.")
     else:
-        customer = CustomerCreate(name=data[0], phone=data[1])
-        return customer
+        return CustomerCreate(name=data[0], phone=data[1])
     
-def get_item_service(id: str, db: sqlite3.Connection = Depends(get_db)):
+def get_item_service(id: str):
     """Retrieves a JSON representation of an item in the DB"""
-    cursor = db.cursor()
-
     # Retrieve the customer
     cursor.execute("SELECT name, price FROM items where id = ?", (id,))
     data = cursor.fetchone()
-    cursor.close()
     if data is None:
-        return data
+        raise HTTPException(status_code=404, detail=f"Item {id} not found.")
     else:
         item = ItemCreate(name=data[0], price=data[1])
         return item
     
-def get_item_given_name(item_name: str, db: sqlite3.Connection = Depends(get_db)):
+def get_item_given_name(item_name: str):
     """Given a name, retrieves an item id and its price from the DB"""
-    cursor = db.cursor()
-
     # Retrieve the customer
     cursor.execute("SELECT id, price FROM items where name = ?", (item_name,))
     data = cursor.fetchone()
@@ -123,10 +117,7 @@ def get_item_given_name(item_name: str, db: sqlite3.Connection = Depends(get_db)
         item_price = data[1]
 
         # print(f"Item id {item_id} has name {item_name} with prince {item_price}.")
-        item = Item(id=item_id, name=item_name, price=item_price)
-
-    cursor.close()
-    return item
+        return Item(id=item_id, name=item_name, price=item_price)
     
 def format_phone_number(phone_number):
         """Formats the input phone number string to a standard 'xxx-xxx-xxxx' format."""
@@ -151,7 +142,7 @@ def validate_customer_phone_length(phone):
 def format_price(price):
     return round(price, 2)
 
-def create_customer_service(customer_create: CustomerCreate, db: sqlite3.Connection = Depends(get_db)):
+def create_customer_service(customer_create: CustomerCreate):
     """Creates a customer in the DB given a JSON representation"""
     name = customer_create.name
     phone = customer_create.phone
@@ -163,58 +154,52 @@ def create_customer_service(customer_create: CustomerCreate, db: sqlite3.Connect
         raise HTTPException(status_code=400, detail=f"Customer Phone is not of required length {REQUIRED_PHONE_LENGTH}.")
     phone = format_phone_number(phone)
 
-    cursor = db.cursor()
-
     # Insert a new row
     cursor.execute("INSERT INTO customers(name, phone) VALUES (?, ?);", (name, phone))
     
     # Commit the transaction
-    db.commit()
+    connection.commit()
 
     last_id = cursor.lastrowid
-    customer = Customer(id=last_id, name=name, phone=phone)
-
-    cursor.close()
-    return customer
+    return Customer(id=last_id, name=name, phone=phone)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.post("/customers")
-async def create_customer(customer_create: CustomerCreate, db: sqlite3.Connection = Depends(get_db)):
+async def create_customer(customer_create: CustomerCreate):
     """Creates a customer in the DB given a JSON representation"""
-    return create_customer_service(customer_create, db)
+    return create_customer_service(customer_create)
 
 @app.get("/customers/{id}")
-async def get_customer(id: str, db: sqlite3.Connection = Depends(get_db)):
+async def get_customer(id: str):
     """Retrieves a JSON representation of a customer in the DB"""
-    customer = get_customer_service(id, db)
+    customer = get_customer_service(id)
     if customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
     else:
         return customer
     
 @app.delete("/customers/{id}")
-async def delete_customer(id: str, db: sqlite3.Connection = Depends(get_db)):
+async def delete_customer(id: str):
     """Deletes a customer in the DB"""
-    customer = get_customer_service(id, db)
+    customer = get_customer_service(id)
     if customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
-    
-    cursor = db.cursor()
+
     cursor.execute("DELETE FROM customers WHERE id = ?", (id,))
-    db.commit()
+    connection.commit()
+
     if cursor.rowcount == 0:
         cursor.close()
         raise HTTPException(status_code=404, detail="Customer not found")
-    cursor.close()
     return f"Successfully deleted customer id {id} with name {customer.name} and phone {customer.phone}."
 
 @app.put("/customers/{id}")
-async def update_customer(id: str, customer_update: CustomerUpdate, db: sqlite3.Connection = Depends(get_db)):
+async def update_customer(id: str, customer_update: CustomerUpdate):
     """Updates a customer in the DB given a JSON representation"""
-    customer = get_customer_service(id, db)
+    customer = get_customer_service(id)
     if customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
     
@@ -232,7 +217,6 @@ async def update_customer(id: str, customer_update: CustomerUpdate, db: sqlite3.
             raise HTTPException(status_code=400, detail=f"Customer Phone is not of required length {REQUIRED_PHONE_LENGTH}.")
         new_phone = format_phone_number(new_phone)
 
-    cursor = db.cursor()
     if (new_name is not None and new_name != "" and new_phone is None):
         cursor.execute(
             "UPDATE customers SET name = ? WHERE id = ?",
@@ -252,16 +236,14 @@ async def update_customer(id: str, customer_update: CustomerUpdate, db: sqlite3.
         )
         result_detail = f"Successfully updated customer id {id} with old name {old_name} and phone {old_phone} to new name {new_name} and phone {new_phone}." 
 
-    db.commit()
+    connection.commit()
     if cursor.rowcount == 0:
         cursor.close()
         raise HTTPException(status_code=404, detail="Customer not found")
-    cursor.close()
-
     return result_detail
 
 @app.post("/items")
-async def create_item(item_create: ItemCreate, db: sqlite3.Connection = Depends(get_db)):
+async def create_item(item_create: ItemCreate):
     """Creates an item in the DB given a JSON representation"""
     name = item_create.name
     price = format_price(item_create.price)
@@ -269,49 +251,41 @@ async def create_item(item_create: ItemCreate, db: sqlite3.Connection = Depends(
     if (not validate_customer_name_length(name)):
         raise HTTPException(status_code=400, detail=f"Item name is beyond the maximum allowed length of {MAXIMUM_NAME_LENGTH}.")
 
-    cursor = db.cursor()
-
     # Insert a new row
     cursor.execute("INSERT INTO items(name, price) VALUES (?, ?);", (name, price))
     
     # Commit the transaction
-    db.commit()
+    connection.commit()
 
     last_id = cursor.lastrowid
-    item = Item(id=last_id, name=name, price=price)
-
-    cursor.close()
-    return item
+    return Item(id=last_id, name=name, price=price)
 
 @app.get("/items/{id}")
-async def get_item(id: str, db: sqlite3.Connection = Depends(get_db)):
+async def get_item(id: str):
     """Retrieves a JSON representation of an item in the DB"""
-    item = get_item_service(id, db)
+    item = get_item_service(id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     else:
         return item
     
 @app.delete("/items/{id}")
-def delete_item(id: str, db: sqlite3.Connection = Depends(get_db)):
+def delete_item(id: str):
     """Deletes an item in the DB"""
-    item = get_item_service(id, db)
+    item = get_item_service(id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     
-    cursor = db.cursor()
     cursor.execute("DELETE FROM items WHERE id = ?", (id,))
-    db.commit()
+    connection.commit()
     if cursor.rowcount == 0:
-        cursor.close()
         raise HTTPException(status_code=404, detail="Item not found")
-    cursor.close()
     return f"Successfully deleted item id {id} with name {item.name} and price {item.price}."
 
 @app.put("/items/{id}")
-def update_item(id: str, item_update: ItemUpdate, db: sqlite3.Connection = Depends(get_db)):
+def update_item(id: str, item_update: ItemUpdate):
     """Updates an item in the DB given a JSON representation"""
-    item = get_item_service(id, db)
+    item = get_item_service(id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     
@@ -327,7 +301,6 @@ def update_item(id: str, item_update: ItemUpdate, db: sqlite3.Connection = Depen
     if (new_price is not None):
         new_price = format_price(new_price)
 
-    cursor = db.cursor()
     if (new_name is not None and new_name != "" and new_price == 0.00):
         cursor.execute(
             "UPDATE items SET name = ? WHERE id = ?",
@@ -347,20 +320,17 @@ def update_item(id: str, item_update: ItemUpdate, db: sqlite3.Connection = Depen
         )
         result_detail = f"Successfully updated item id {id} with old name {old_name} and price {old_price} to new name {new_name} and price {new_price}." 
 
-    db.commit()
+    connection.commit()
     if cursor.rowcount == 0:
-        cursor.close()
         raise HTTPException(status_code=404, detail="Item not found")
-    cursor.close()
 
     return result_detail
 
 @app.post("/orders")
-async def create_order(order_create: OrderCreate, db: sqlite3.Connection = Depends(get_db)):
+async def create_order(order_create: OrderCreate):
     """Creates an order in the DB given a JSON representation"""
     customer_name = order_create.name
     customer_phone = order_create.phone
-    cursor = db.cursor()
 
     cursor.execute("SELECT id FROM customers WHERE name = ? AND phone = ?", (order_create.name, order_create.phone,))
     data = cursor.fetchone()
@@ -368,25 +338,23 @@ async def create_order(order_create: OrderCreate, db: sqlite3.Connection = Depen
     if data is None:
         # Insert new customer
         new_customer = CustomerCreate(name=customer_name, phone=customer_phone)
-        cust_id = create_customer_service(new_customer, db).id
+        cust_id = create_customer_service(new_customer).id
     else:
         cust_id = data[0]
     
     # Insert a new row into orders table
     cursor.execute("INSERT INTO orders(cust_id, notes) VALUES (?, ?);", (cust_id, order_create.notes,))
     # Commit the transaction
-    db.commit()
-
+    connection.commit()
     order_id = cursor.lastrowid
-    # print(f"order_id {order_id}")
-    cursor.execute("SELECT * from ORDERS where id = ?", (order_id,))
+
+    cursor.execute("SELECT * from orders where id = ?", (order_id,))
     data = cursor.fetchone()
 
     if data is None:
         raise HTTPException(status_code=404, detail="Order ID {order_id} not found.")
     else: 
         timestamp = data[1]
-        # print(f"Timestamp {timestamp}")
 
     items_list = []
     total = 0
@@ -394,8 +362,7 @@ async def create_order(order_create: OrderCreate, db: sqlite3.Connection = Depen
     # Get item_id and price given name
     for item_ordered in order_create.items:
         item_name = item_ordered.name
-        # print(f"About to get the item given name {item_name}")
-        item = get_item_given_name(item_name, db)
+        item = get_item_given_name(item_name)
         item_price = item.price
         item_quantity = item_ordered.quantity
         item_price_total = item_price * item_quantity
@@ -405,11 +372,50 @@ async def create_order(order_create: OrderCreate, db: sqlite3.Connection = Depen
         for _ in range(item_quantity):
             cursor.execute("INSERT INTO item_list(order_id, item_id) VALUES (?, ?);", (order_id, item.id,))
             # Commit the transaction
-            db.commit()
-            #print(f"Successfully inserted")
+            connection.commit()
 
         # Construct ItemQuantityTotalPrice object
         items_list.append(ItemQuantityTotalPrice(name = item_name, itemPrice = item_price, quantity=item_quantity, itemPriceTotal = item_price_total))
-    # print(f"Items List count is {len(items_list)}")
-    cursor.close()
-    return OrderCreated(id=order_id, timestamp=timestamp, name=customer_name, phone=customer_phone, items=items_list, total=total)
+    return OrderCreated(id=order_id, timestamp=timestamp, items=items_list, total=total)
+
+@app.get("/orders/{id}")
+async def get_order(id: int):
+    """retrieves a JSON representation of an order in the DB"""
+    # Get order by id
+    cursor.execute("SELECT * from orders where id = ?", (id,))
+    order_data = cursor.fetchone()
+
+    if order_data is None:
+        raise HTTPException(status_code=404, detail="Order ID {order_id} not found.")
+    else: 
+        timestamp = order_data[1]
+        customer_id = order_data[2]
+        notes = order_data[3]
+
+    # Get customer by id
+    customer = get_customer_service(customer_id)
+
+    # Get item list by order_id
+    cursor.execute("SELECT * from item_list where order_id = ?", (id,))
+    item_list_data = cursor.fetchall()
+
+    items_map = {}
+    total = 0
+
+    if item_list_data is None:
+        raise HTTPException(status_code=404, detail="Order ID {id} not found in item_list table.")
+    else:
+        for row in item_list_data:
+            item_id = row[1]
+
+            # Get item by id
+            item = get_item_service(item_id)
+            if item_id in items_map:
+                items_quantity_total_price = items_map[item_id]
+                items_quantity_total_price.quantity += 1
+                items_quantity_total_price.item_price_total = item.price * items_quantity_total_price.quantity
+            else:
+                items_map[item_id] = ItemQuantityTotalPrice(name=item.name, itemPrice=item.price, quantity=1, itemPriceTotal=item.price)
+        if len(items_map) > 0:
+            total=sum(item.item_price_total for item in items_map.values())
+    return OrderReturned(id=id, timestamp=timestamp, name=customer.name, phone=customer.phone, notes=notes, items=list(items_map.values()), total=total)
